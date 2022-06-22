5ae6aed731fe8cb5f31f9538fded26a5e86e6656
Bernardo <biesseck@gmail.com>
2022-06-22 17:05:25 -0400

    Add nome da equipe na tela inicial

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Aplicativo - Trabalho Prog III - 2º Bimestre</Text>
+      <Text>Grupo: Bernardo Biesseck</Text>
       <StatusBar style="auto" />
     </View>
   );
